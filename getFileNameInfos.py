a80cfaf19e53f2fa038611adb79e5330c5d376fe
Simon Dusini <simon.dusini@uibk.ac.at>
2025-08-20 14:00:19 +0200

    added extraction of file name parameters

---
+++ getFileNameInfos.py
@@ -0,0 +1,29 @@
+Manufacturers = ["Vishay", "WürthElectronic", "Eaton", "Sech", "Kyocera", "Maxwell"]  # All manufacturers
+
+def getFileNameInfos(filename):
+  nameparts = filename.split("_")
+  typ = nameparts[0]  # e.g., "C" or "ESR"
+  for part in nameparts:
+    if part in Manufacturers:
+      manufacturer = part
+      break
+  for part in nameparts:
+    if part.endswith("F"):
+      capacitance = str(part[:-1])  # Extract the number before "F"
+      break
+
+  for part in nameparts:
+    if "A1" in part or "A2" in part or "A3" in part or "A4" in part:
+      methode = part[0]  # e.g.,A or B
+      klass = part[1]
+    if "B1" in part:
+      methode = part[0]
+      klass = part[1]
+
+
+  return manufacturer, capacitance, typ, methode, klass
+
+
+if __name__ == "__main__":
+  print(getFileNameInfos("C_A2_DUT1_V1_Vishay_25F_05-08-2025"))
+  pass
